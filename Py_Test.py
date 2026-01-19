27a17eb5ba683f4153c7ec9d3771b51d00e44fb8
Nathaniel Jasko <njasko0784@floridapoly.edu>
2026-01-19 00:39:44 +0000

    Finished print homework

---
+++ Py_Test.py
@@ -0,0 +1,7 @@
+my_name = 'Nathaniel Jasko'
+print(my_name)
+print("Use 4 spaces per indentation level")
+print("Python has different implementations, written in other languages")
+print("I hope to become more familar with the python language, " \
+    "understand what makes python unique, "
+    "and gain experience for a professional setting")
