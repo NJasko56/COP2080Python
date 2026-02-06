994aa025b807464c48f009f91f2d5ac0a103256d
Nathaniel Jasko <njasko0784@floridapoly.edu>
2026-02-06 21:21:16 +0000

    Finished L06 participation activity

--- L06_Participate.py
+++ L06_Participate.py
@@ -73,7 +73,7 @@
 # list comprehension vs generator expression
 import sys
 a = [x for x in range(1000000)] #list comp
-b = (x for x in range[1000000])
+b = (x for x in range(1000000))
 print('list comp byte size is ',sys.getsizeof(a))
 print('generator expression byte size is ',sys.getsizeof(b))
 # double click to copy
